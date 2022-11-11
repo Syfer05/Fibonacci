59613f4a4476d9b6116ff66188adba1de56adb70
Syfer05 <syfer@gmx.fr>
2022-11-11 16:39:31 +0100

    Add data verifications

--- main.py
+++ main.py
@@ -1,8 +1,26 @@
 # Python program 
 # Fibonacci Sequence
-  
-val = int(input("Enter the number of items you want: "))
-alpha = [0,1]
-for i in range (1, val):
-    alpha.append(alpha[-2] + alpha[-1])
-print(alpha)
+
+val1 = ''
+def item():
+    try:
+        val1 = int(input("Enter the number of items you want between [2:20]: "))
+    except ValueError:
+        print("That's not an int!")   
+        item()
+        
+    if val1 not in range(2, 21):
+        item()
+    else:
+        sequence(val1)
+
+    
+
+def sequence(val2):
+    alpha = [0,1]
+    for i in range (1, val2):
+        alpha.append(alpha[-2] + alpha[-1])
+    print(alpha)
+
+
+item()
